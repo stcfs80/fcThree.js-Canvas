0598bf35aa9c1f7d908a206b501a024682688366
stcfs80 <stcfs80@gmail.com>
2024-11-11 14:32:45 +0900

    gooey효과 완성

--- canvas1/two.js
+++ canvas1/two.js
@@ -2,17 +2,64 @@ const canvas =document.querySelector('canvas')
 
 const ctx = canvas.getContext('2d')
 const dpr = window.devicePixelRatio
-const canvasWidth = innerWidth
-const canvasHeight = innerHeight
+let canvasWidth
+let canvasHeight
+let particles 
 
-canvas.style.width = canvasWidth + 'px'
-canvas.style.height = canvasHeight + 'px'
+function init(){
+    
+    canvasWidth = innerWidth
+    canvasHeight = innerHeight
+    canvas.style.width = canvasWidth + 'px'
+    canvas.style.height = canvasHeight + 'px'
 
-canvas.width = canvasWidth * dpr
-canvas.height = canvasHeight * dpr
-ctx.scale(dpr,dpr)
+    canvas.width = canvasWidth * dpr
+    canvas.height = canvasHeight * dpr
+    ctx.scale(dpr,dpr)
+    particles = []
+    const TOTAL = canvasWidth / 30
 
+    for(let i = 0; i < TOTAL; i++){
+    const x = randomNumBetween(0, canvasWidth)
+    const y = randomNumBetween(0, canvasHeight)
+    const radius = randomNumBetween(50,100)
+    const vy = randomNumBetween(1,5)
+    const particle = new Particle(x,y, radius, vy)
+    particles.push(particle)
+}
+}
 
+/* dat.gui 테스트 svg 필터 셋팅 */
+const controls = new function() {
+    this.blurValue = 40
+    this.alphaChannel = 100
+    this.alphaOffset = -23
+    this.acc = 1.03
+}
+
+const feGaussianBlur = document.querySelector('feGaussianBlur');
+const feColorMatrix = document.querySelector('feColorMatrix')
+
+let gui = new dat.GUI()
+
+const f1 = gui.addFolder('Gooey Effect')
+f1.open()
+
+f1.add(controls, 'blurValue', 0, 100).onChange(value =>{
+    feGaussianBlur.setAttribute('stDeviation', value)
+});
+
+f1.add(controls,'alphaChannel',1,200).onChange(value =>{
+    feColorMatrix.setAttribute('values',`1 0 0 0 0   0 1 0 0 0   0 0 1 0 0  0 0 0 ${value} ${controls.alphaOffset}`)
+})
+f1.add(controls,'alphaOffset',-40,40).onChange(value =>{
+    feColorMatrix.setAttribute('values',`1 0 0 0 0   0 1 0 0 0   0 0 1 0 0  0 0 0 ${controls.alphaChannel} ${value}`)
+})
+const f2 = gui.addFolder('particle Property')
+f2.open()
+f2.add(controls,'acc',1, 1.5, 0.01).onChange(value =>{
+    particles.forEach(particle => particle.acc = value)
+})
 // 각도 1도는 3.14를 180으로 나눈 값과 같다.
 
 
@@ -42,20 +89,11 @@ const x = 100
 const y = 100
 const radius = 50
 const particle = new Particle(x, y, radius)
-const TOTAL = 20
+
 const randomNumBetween = (min, max) =>{
     return Math.random() * (max - min + 1) + min
 }
-let particles = []
 
-for(let i = 0; i < TOTAL; i++){
-    const x = randomNumBetween(0, canvasWidth)
-    const y = randomNumBetween(0, canvasHeight)
-    const radius = randomNumBetween(50,100)
-    const vy = randomNumBetween(1,5)
-    const particle = new Particle(x,y, radius, vy)
-    particles.push(particle)
-}
 
 
 let interval = 1000 / 60
@@ -88,4 +126,11 @@ function animate(){
     then = now - (delta % interval)
 }
 //주사율 , fpx , frame per second 1초에 리퀘스트 애니메이션 프레임을 몇번을 실행시킬까 ?
-animate()
+
+window.addEventListener('load',()=>{
+    init()
+    animate()
+})
+window.addEventListener('resize',()=>{
+    init()
+})
